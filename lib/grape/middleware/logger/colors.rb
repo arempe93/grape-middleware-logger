b35ad76f98c47dbc2bed827342d0d6b72077982e
Andrew Rempe <arempe@k2p-ed.com>
2017-07-08 22:33:34 -0400

    fix bold style rendering

--- lib/grape/middleware/logger/colors.rb
+++ lib/grape/middleware/logger/colors.rb
@@ -4,20 +4,20 @@ module Colors
   ESCAPE = "\e["
   RESET = '0m'
   CODES = {
-    black: '30m',
-    red: '31m',
-    green: '32m',
-    yellow: '33m',
-    blue: '34m',
-    magenta: '35m',
-    cyan: '36m'
+    black: '30',
+    red: '31',
+    green: '32',
+    yellow: '33',
+    blue: '34',
+    magenta: '35',
+    cyan: '36'
   }
 
   CODES.each do |color, code|
     define_method(color) do |string, bold: false|
       attributes = code
       attributes << ';1' if bold
-      "#{ESCAPE}#{attributes}#{string}#{ESCAPE}#{RESET}"
+      "#{ESCAPE}#{attributes}m#{string}#{ESCAPE}#{RESET}"
     end
   end
 end
