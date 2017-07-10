989e08031fc42f26eef2daef8388f2fb033ea3c4
Andrew Rempe <andrewrempe@gmail.com>
2017-07-10 11:57:48 -0400

    update after failure to handle nil response

--- lib/grape/middleware/logger.rb
+++ lib/grape/middleware/logger.rb
@@ -97,7 +97,8 @@ class Grape::Middleware::Logger < Grape::Middleware::Globals
   private
 
   def after_failure(status:, response:)
-    logger.info Colors.yellow("  Failing with #{status} (#{response.fetch(:message, response || '<NO MESSAGE>')})")
+    message = response ? response.fetch(:message, '<NO MESSAGE>') : <NIL RESPONSE>
+    logger.info Colors.yellow("  Failing with #{status} (#{message})")
     after(status)
   end
 
